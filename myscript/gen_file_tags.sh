c0c41dc74abac54b4e6ce634b4d5118314f477d8
Hughes Yang <hyang@advaoptical.com>
2017-12-25 08:56:27 +0800

    delete unused script

--- myscript/gen_file_tags.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# generate tag file for lookupfile plugin
-echo -e "!_TAG_FILE_SORTED\t2\t/2=foldcase/" > filenametags
-#find . -not -regex '.*\.\(git\|svn\)' -type f -printf "%f\t%p\t1\n" | \
-find . -not -regex '.*\.\(git\|svn\)' -type f -printf "%f\t%p\t1\n" | \
-    sort -f >> filenametags
-
-
