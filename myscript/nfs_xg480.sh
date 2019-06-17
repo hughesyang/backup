0b28382fe79d89b7e82139152cb9743b97548bed
Hughes Yang <hyang@advaoptical.com>
2019-06-17 18:06:07 +0800

    update scripts

---
+++ myscript/nfs_xg480.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# The script copy qumran scripts to xg480
+
+if [ $# -ne "1" ]; then
+  echo "Usage: `basename $0` <ip>"
+  exit
+fi
+
+BOX_ADDR=$1
+BOX_LOGIN=root@${BOX_ADDR}
+
+# ----------------------------------------------------------------------------------
+function log()
+{
+  echo $(date +%T) "$*"
+}
+# ----------------------------------------------------------------------------------
+
+# commands
+SCP_CMD="sudo scp -P 614"
+SSH_CMD="sudo ssh -p 614"
+
+# local path storing scripts
+LOCAL_PATH=/home/hughes/upload/dnx
+
+# ----------------------------------------------------------------------------------
+log " -- enable r/w access"
+${SSH_CMD} ${BOX_LOGIN} -o "StrictHostKeyChecking no" 'mount -o remount,rw /opt/adva; mount -o remount,rw /lib/modules/'
+
+log " -- create/copy files"
+${SSH_CMD} ${BOX_LOGIN} 'cd /opt/adva; mkdir -p qumran; cd qumran; mkdir -p dnx; cd dnx; mkdir -p rmt;'
+
+cd ${LOCAL_PATH}
+${SCP_CMD} * ${BOX_LOGIN}:/opt/adva/qumran/dnx
+${SSH_CMD} ${BOX_LOGIN} 'cd /opt/adva/qumran/dnx; sudo cp bashrc /etc/bash/'
+
+log "*** Done ***"
+exit 0
